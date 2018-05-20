0366c836ae3a9824b6948e4a85a2d7b1669115fc
ravi.malviya <ravi.malviya@hestabit.in>
2018-05-20 23:13:44 +0530

    fcgvbhnm

---
+++ api_test.php
@@ -0,0 +1,29 @@
+
+
+GET&https%3A%2F%2enterprise-sandbox-uk.simprosuite.com%2Fapi%2Foauth%2Frequest_token.php&oauth_callback%3Doob%26dokkit-integration%3Duser-access-token-example%26oauth_nonce%3D3wwez895%26oauth_signature_method%3DRSA-SHA1%26oauth_timestamp%3D1526383932%26oauth_version%3D1.0
+
+
+Zr08HFzbiRqSfVX/g+rI1BInJq8=
+
+curl -X GET https://enterprise-sandbox-uk.simprosuite.com/api/oauth/request_token.php -H "authorization: OAuth
+oauth_callback=oob,oauth_consumer_key=dokkit-integration,oauth_nonce=aaaaa3,oauth_signature_method=RSA-SHA
+1,oauth_timestamp=1526387448,oauth_version=1.0,oauth_signature=a5KKQMfGE7V6ft4bIopvNX7DpMI%3D"
+
+
+
+ oauth_callback=oob&oauth_version=1.0&oauth_nonce=aaaaa4&oauth_signature_method=RSA-SHA1&oauth_consumer_key=dokkit-integration&oauth_timestamp=1526388641
+
+ GET&https://enterprise-sandbox-uk.simprosuite.com/api/oauth/request_token.php&oauth_callback=oob&oauth_version=1.0&oauth_nonce=aaaaa4&oauth_signature_method=RSA-SHA1&oauth_consumer_key=dokkit-integration&oauth_timestamp=1526388641
+
+
+
+17-05-2018
+
+ GET&https%3A%2F%2Fenterprise-sandbox-uk.simprosuite.com%2Fapi%2Foauth%2Frequest_token.php&oauth_callback%3Doob%26oauth_version%3D1.0%26oauth_nonce%3D3ffe99%26oauth_signature_method%3DRSA-SHA1%26oauth_consumer_key%3Ddokkit-integration%26oauth_timestamp%3D1526532680
+
+Input text:--
+
+ GET&https%3A%2F%2Fenterprise-sandbox-uk.simprosuite.com%2Fapi%2Foauth%2Frequest_token.php&oauth_callback%3Doob%26oauth_consumer_key%3Ddokkit-integration%26oauth_nonce%3D3ffe66%26oauth_signature_method%3DRSA
+-SHA1%26oauth_timestamp%3D1526536879%26oauth_version%3D1.0
+
+Key:--
---
+++ requests.php
@@ -0,0 +1,19 @@
+https://enterprise-sandbox-uk.simprosuite.com/api/oauth/request_token.php&oauth_callback=oob&oauth_consumer_key=dokkit-integration&oauth_version=1.0&oauth_nonce=aaaaa6&oauth_signature_method=RSA-SHA1&oauth_timestamp=1526531249&oauth_signature=jy4NFMTNxz6rmhHtvy5YnFNpucc%3D
+
+
+
+https://aglcom.simprosuite.com/api/?oauth_consumer_key=aglapp-simpro&oauth_token&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1525345902&oauth_nonce=y2FfM2&oauth_version=1.0&oauth_signature=i1SNBlW6GXMEYuYeCBA9WvcTnvI=
+
+oauth_consumer_key
+
+https://enterprise-sandbox-uk.simprosuite.com/api/oauth/request_token.php?oauth_consumer_key=dokkit-integration&oauth_signature_method=RSA-SHA1&oauth_timestamp=1526531249&oauth_nonce=123456&oauth_version=1.0&oauth_signature=jy4NFMTNxz6rmhHtvy5YnFNpucc%3D
+
+
+curl -X GET https://enterprise-sandbox-uk.simprosuite.com/api/oauth/request_token.php -H "authorization: OAuth
+oauth_callback=oob,oauth_consumer_key=user-access-token-example,oauth_nonce=3ffe5895,oauth_signature_method=HMAC-SHA
+1,oauth_timestamp=1508481752,oauth_version=1.0,oauth_signature=HYUcWDXuafUzH2%2Ftdw%2B4B2sJxzA%3D"
+
+
+Request to send:--
+
+curl -X GET https://enterprise-sandbox-uk.simprosuite.com/api/oauth/request_token.php -H "authorization: OAuth oauth_callback=oob,oauth_consumer_key=dokkit-integration,oauth_nonce=3ffe66,oauth_signature_method=RSA-SHA1,oauth_timestamp=1526536879,oauth_version=1.0,oauth_signature=kyERkWGIT%2FohtXr%2FVvucVDAE%2BRs%3D"
